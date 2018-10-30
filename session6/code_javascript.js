ffee55c1cc97f376ab0e054778ad5eca05a79607
phuc1640 <tasinhphuc@gmail.com>
2018-10-30 17:56:03 +0700

    thanh cong

---
+++ session6/code_javascript.js
@@ -0,0 +1,25 @@
+var n = 9;
+document.write(n + ' la ');
+if(n%2==0){
+	document.write('so chan ');
+}else{
+	document.write('so le ');
+	
+	if(n%3==0){
+		document.write('chia het cho 3');
+		document.write('<br>');
+	}else{
+		document.write('khong chia het cho 3');
+		document.write('<br>');
+	}
+}
+
+if (n>=1 && n<=12) {
+	document.write(n + ' la ');
+	document.write('thang trong nam');
+	document.write('<br>');
+}else{
+	document.write(n + ' khong la ');
+	document.write('thang trong nam');
+	document.write('<br>');
+}
